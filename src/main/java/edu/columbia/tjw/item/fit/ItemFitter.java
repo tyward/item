5e8f82e9c54a40a51a3d765a4584b0712f80ef46
tyler <tyler@192.168.1.5>
2014-10-04 15:45:14 -0400

    Adding sources and javadocs plugins

---
+++ src/main/java/edu/columbia/tjw/item/fit/ItemFitter.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2014 Tyler Ward.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * This code is part of the reference implementation of http://arxiv.org/abs/1409.6075
+ * 
+ * This is provided as an example to help in the understanding of the ITEM model system.
+ */
+package edu.columbia.tjw.item.fit;
+
+import edu.columbia.tjw.item.ItemCurveType;
+import edu.columbia.tjw.item.ItemGridFactory;
+import edu.columbia.tjw.item.ItemModel;
+import edu.columbia.tjw.item.ItemParameters;
+import edu.columbia.tjw.item.ItemRegressor;
+import edu.columbia.tjw.item.ItemStatus;
+
+/**
+ *
+ * @author tyler
+ * @param <S>
+ * @param <R>
+ * @param <T>
+ */
+public final class ItemFitter<S extends ItemStatus<S>, R extends ItemRegressor<R>, T extends ItemCurveType<T>>
+{
+
+    public ItemFitter(final R interceptRegressor_)
+    {
+
+    }
+
+    public ItemModel<S, R, T> generateBaseModel(final ItemGridFactory<S, R, T> gridFactory_)
+    {
+
+        return null;
+    }
+
+    public ItemModel<S, R, T> expandModel(final ItemParameters<S, R, T> params_, final ItemGridFactory<S, R, T> gridFactory_)
+    {
+        
+        
+        
+        
+        return null;
+    }
+}
