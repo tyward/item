7a3fad63fbddfafed251ab22ea733e8f940675b4
tyler <tyler@192.168.1.13>
2017-04-29 20:21:07 -0400

    Minor cleanup.

--- src/main/java/edu/columbia/tjw/item/ItemSettings.java
+++ src/main/java/edu/columbia/tjw/item/ItemSettings.java
@@ -19,7 +19,6 @@
  */
 package edu.columbia.tjw.item;
 
-import edu.columbia.tjw.item.util.random.PrngType;
 import edu.columbia.tjw.item.util.random.RandomTool;
 import java.io.Serializable;
 import java.util.Random;
@@ -31,7 +30,7 @@ import java.util.Random;
  * Generally, don't change these unless you know what you're doing.
  *
  * Also, all the members are final so that this thing is threadsafe. Use the
- * with* methods if you need to get settings with non-default values.
+ * builder to make adjusted versions of this class.
  *
  * One word of warning, these will share a single Random, typically this will
  * still be threadsafe, but not always. Be careful about that.
@@ -40,6 +39,8 @@ import java.util.Random;
  */
 public final class ItemSettings implements Serializable
 {
+    private static final long serialVersionUID = 0x2bc96e9485221bf6L;
+
     private static final double STANDARD_AIC_CUTOFF = -5.0;
     private static final int BLOCK_SIZE = 200 * 1000;
     private static final int THREAD_BLOCK_SIZE = 10 * 1000;
@@ -51,7 +52,7 @@ public final class ItemSettings implements Serializable
     private static final double Z_SCORE_CUTOFF = 1.0;
     private static final boolean DEFAULT_VALIDATE = true;
 
-    private static final long serialVersionUID = 0x2bc96e9485221bf6L;
+    private static final ItemSettings DEFAULT = new ItemSettings();
 
     private final Random _rand;
     private final boolean _randomShuffle;
@@ -62,9 +63,9 @@ public final class ItemSettings implements Serializable
     private final boolean _boundCentrality;
     private final int _polishMultStartPoints;
 
-    private final int _minCalibrationCount = 1;
-    private final double _improvementRatio = 0.2;
-    private final double _exhaustiveImprovementLimit = 0.05;
+    private final int _minCalibrationCount;
+    private final double _improvementRatio;
+    private final double _exhaustiveImprovementLimit;
 
     private final double _aicCutoff;
 
@@ -81,40 +82,42 @@ public final class ItemSettings implements Serializable
 
     public ItemSettings()
     {
-        this(true, STANDARD_AIC_CUTOFF, RandomTool.getRandom(PrngType.STANDARD), BLOCK_SIZE, THREAD_BLOCK_SIZE, DEFAULT_USE_THREADING, DEFAULT_POLISH_STARTING_PARAMS, ALLOW_INTERACTION_CURVES);
+        _rand = RandomTool.getRandom();
+        _randomShuffle = true;
+        _useThreading = DEFAULT_USE_THREADING;
+        _approximateDerivatives = false;
+        _polishStartingParams = DEFAULT_POLISH_STARTING_PARAMS;
+        _allowInteractionCurves = ALLOW_INTERACTION_CURVES;
+        _boundCentrality = CENTRALITY_BOUND;
+        _polishMultStartPoints = POLISH_MULTI_START_POINTS;
+        _minCalibrationCount = 1;
+        _improvementRatio = 0.2;
+        _exhaustiveImprovementLimit = 0.05;
+        _aicCutoff = STANDARD_AIC_CUTOFF;
+        _zScoreCutoff = Z_SCORE_CUTOFF;
+        _blockSize = BLOCK_SIZE;
+        _threadBlockSize = THREAD_BLOCK_SIZE;
+        _validate = DEFAULT_VALIDATE;
     }
 
-    public ItemSettings(final boolean randomShuffle_, final double aicCutoff_, final Random rand_, final int blockSize_, final int threadBlockSize_, final boolean useThreading_,
-            final boolean polishStartingParams_, final boolean allowInteractionCurves_)
+    public ItemSettings(final ItemSettingsBuilder builder_)
     {
-        if (aicCutoff_ > 0.0)
-        {
-            throw new IllegalArgumentException("The AIC cutoff must be negative: " + aicCutoff_);
-        }
-
-        if (threadBlockSize_ < 100)
-        {
-            throw new IllegalArgumentException("Thread block size should not be too small: " + threadBlockSize_);
-        }
-        if (blockSize_ < threadBlockSize_)
-        {
-            throw new IllegalArgumentException("Block size cannot be less than ThreadBlockSize: " + blockSize_);
-        }
-
-        _rand = rand_;
-        _randomShuffle = randomShuffle_;
-        _aicCutoff = aicCutoff_;
-        _blockSize = blockSize_;
-        _threadBlockSize = threadBlockSize_;
-        _useThreading = useThreading_;
-        _approximateDerivatives = false;
-        _polishStartingParams = polishStartingParams_;
-        _polishMultStartPoints = POLISH_MULTI_START_POINTS;
-        _allowInteractionCurves = allowInteractionCurves_;
-        _boundCentrality = CENTRALITY_BOUND;
-
-        _zScoreCutoff = Z_SCORE_CUTOFF;
-        _validate = DEFAULT_VALIDATE;
+        _rand = builder_.getRand();
+        _randomShuffle = builder_.isRandomShuffle();
+        _useThreading = builder_.isUseThreading();
+        _approximateDerivatives = builder_.isApproximateDerivatives();
+        _polishStartingParams = builder_.isPolishStartingParams();
+        _allowInteractionCurves = builder_.isAllowInteractionCurves();
+        _boundCentrality = builder_.isBoundCentrality();
+        _polishMultStartPoints = builder_.getPolishMultStartPoints();
+        _minCalibrationCount = builder_.getMinCalibrationCount();
+        _improvementRatio = builder_.getImprovementRatio();
+        _exhaustiveImprovementLimit = builder_.getExhaustiveImprovementLimit();
+        _aicCutoff = builder_.getAicCutoff();
+        _zScoreCutoff = builder_.getzScoreCutoff();
+        _blockSize = builder_.getBlockSize();
+        _threadBlockSize = builder_.getThreadBlockSize();
+        _validate = builder_.isValidate();
     }
 
     public double getExhaustiveImprovementLimit()
@@ -147,22 +150,9 @@ public final class ItemSettings implements Serializable
         return _validate;
     }
 
-    public ItemSettings withAllowInteractionCurves(final boolean allowInteractionCurves_)
-    {
-        //return new ItemSettings(this.isRandomShuffle(), this.getAicCutoff(), this.getRandom(), this.getBlockSize(), this.getThreadBlockSize(), this.getUseThreading(), this.getPolishStartingParams());
-        return new ItemSettings(this.isRandomShuffle(), this.getAicCutoff(), this.getRandom(), this.getBlockSize(), this.getThreadBlockSize(), this.getUseThreading(),
-                this.getPolishStartingParams(), this.getAllowInteractionCurves());
-    }
-
     public boolean getAllowInteractionCurves()
     {
         return _allowInteractionCurves;
-    }
-
-    public ItemSettings withPolishStartingParams(final boolean polishStartingParams_)
-    {
-        return new ItemSettings(this.isRandomShuffle(), this.getAicCutoff(), this.getRandom(), this.getBlockSize(), this.getThreadBlockSize(), this.getUseThreading(),
-                polishStartingParams_, this.getAllowInteractionCurves());
     }
 
     public boolean getPolishStartingParams()
@@ -170,23 +160,9 @@ public final class ItemSettings implements Serializable
         return _polishStartingParams;
     }
 
-    public ItemSettings withUseThreading(final boolean useThreading_)
-    {
-        //return new ItemSettings(this.isRandomShuffle(), this.getAicCutoff(), this.getRandom(), this.getBlockSize(), this.getThreadBlockSize(), this.getUseThreading(), this.getPolishStartingParams());
-        return new ItemSettings(this.isRandomShuffle(), this.getAicCutoff(), this.getRandom(), this.getBlockSize(), this.getThreadBlockSize(), useThreading_,
-                this.getPolishStartingParams(), this.getAllowInteractionCurves());
-    }
-
     public boolean getUseThreading()
     {
         return _useThreading;
-    }
-
-    public ItemSettings withBlockSize(final int blockSize_)
-    {
-        //return new ItemSettings(this.isRandomShuffle(), this.getAicCutoff(), this.getRandom(), this.getBlockSize(), this.getThreadBlockSize(), this.getUseThreading(), this.getPolishStartingParams());
-        return new ItemSettings(this.isRandomShuffle(), this.getAicCutoff(), this.getRandom(), blockSize_, this.getThreadBlockSize(), this.getUseThreading(),
-                this.getPolishStartingParams(), this.getAllowInteractionCurves());
     }
 
     public int getBlockSize()
@@ -194,23 +170,9 @@ public final class ItemSettings implements Serializable
         return _blockSize;
     }
 
-    public ItemSettings withThreadBlockSize(final int threadBlockSize_)
-    {
-        //return new ItemSettings(this.isRandomShuffle(), this.getAicCutoff(), this.getRandom(), this.getBlockSize(), this.getThreadBlockSize(), this.getUseThreading(), this.getPolishStartingParams());
-        return new ItemSettings(this.isRandomShuffle(), this.getAicCutoff(), this.getRandom(), this.getBlockSize(), threadBlockSize_, this.getUseThreading(),
-                this.getPolishStartingParams(), this.getAllowInteractionCurves());
-    }
-
     public int getThreadBlockSize()
     {
         return _threadBlockSize;
-    }
-
-    public ItemSettings withAicCutoff(final double cutoff_)
-    {
-        //return new ItemSettings(this.isRandomShuffle(), this.getAicCutoff(), this.getRandom(), this.getBlockSize(), this.getThreadBlockSize(), this.getUseThreading(), this.getPolishStartingParams());
-        return new ItemSettings(this.isRandomShuffle(), cutoff_, this.getRandom(), this.getBlockSize(), this.getThreadBlockSize(), this.getUseThreading(),
-                this.getPolishStartingParams(), this.getAllowInteractionCurves());
     }
 
     public double getAicCutoff()
@@ -218,23 +180,9 @@ public final class ItemSettings implements Serializable
         return _aicCutoff;
     }
 
-    public ItemSettings withRandomShuffle(final boolean randomShuffle_)
-    {
-        //return new ItemSettings(this.isRandomShuffle(), this.getAicCutoff(), this.getRandom(), this.getBlockSize(), this.getThreadBlockSize(), this.getUseThreading(), this.getPolishStartingParams());
-        return new ItemSettings(randomShuffle_, this.getAicCutoff(), this.getRandom(), this.getBlockSize(), this.getThreadBlockSize(), this.getUseThreading(),
-                this.getPolishStartingParams(), this.getAllowInteractionCurves());
-    }
-
     public boolean isRandomShuffle()
     {
         return _randomShuffle;
-    }
-
-    public ItemSettings withRandom(final Random random_)
-    {
-        //return new ItemSettings(this.isRandomShuffle(), this.getAicCutoff(), this.getRandom(), this.getBlockSize(), this.getThreadBlockSize(), this.getUseThreading(), this.getPolishStartingParams());
-        return new ItemSettings(this.isRandomShuffle(), this.getAicCutoff(), random_, this.getBlockSize(), this.getThreadBlockSize(), this.getUseThreading(),
-                this.getPolishStartingParams(), this.getAllowInteractionCurves());
     }
 
     public Random getRandom()
@@ -251,4 +199,252 @@ public final class ItemSettings implements Serializable
     {
         return _approximateDerivatives;
     }
+
+    public ItemSettingsBuilder makeBuilder()
+    {
+        return new ItemSettingsBuilder(this);
+    }
+
+    public final class ItemSettingsBuilder
+    {
+        private Random _rand;
+        private boolean _randomShuffle;
+        private boolean _useThreading;
+        private boolean _approximateDerivatives;
+        private boolean _polishStartingParams;
+        private boolean _allowInteractionCurves;
+        private boolean _boundCentrality;
+        private int _polishMultStartPoints;
+        private int _minCalibrationCount;
+        private double _improvementRatio;
+        private double _exhaustiveImprovementLimit;
+        private double _aicCutoff;
+        private double _zScoreCutoff;
+        private int _blockSize;
+        private int _threadBlockSize;
+        private boolean _validate;
+
+        public ItemSettingsBuilder()
+        {
+            this(DEFAULT);
+        }
+
+        private ItemSettingsBuilder(final ItemSettings base_)
+        {
+            _rand = base_.getRandom();
+            _randomShuffle = base_.isRandomShuffle();
+            _useThreading = base_.getUseThreading();
+            _approximateDerivatives = base_.approximateDerivatives();
+            _polishStartingParams = base_.getPolishStartingParams();
+            _allowInteractionCurves = base_.getAllowInteractionCurves();
+            _boundCentrality = base_.getBoundCentrality();
+            _polishMultStartPoints = base_.getPolishMultiStartPoints();
+            _minCalibrationCount = base_.getCalibrateSize();
+            _improvementRatio = base_.getImprovementRatio();
+            _exhaustiveImprovementLimit = base_.getExhaustiveImprovementLimit();
+            _aicCutoff = base_.getAicCutoff();
+            _zScoreCutoff = base_.getZScoreCutoff();
+            _blockSize = base_.getBlockSize();
+            _threadBlockSize = base_.getThreadBlockSize();
+            _validate = base_.getDoValidate();
+        }
+
+        public ItemSettings build()
+        {
+            return new ItemSettings(this);
+        }
+
+        public Random getRand()
+        {
+            return _rand;
+        }
+
+        public boolean isRandomShuffle()
+        {
+            return _randomShuffle;
+        }
+
+        public boolean isUseThreading()
+        {
+            return _useThreading;
+        }
+
+        public boolean isApproximateDerivatives()
+        {
+            return _approximateDerivatives;
+        }
+
+        public boolean isPolishStartingParams()
+        {
+            return _polishStartingParams;
+        }
+
+        public boolean isAllowInteractionCurves()
+        {
+            return _allowInteractionCurves;
+        }
+
+        public boolean isBoundCentrality()
+        {
+            return _boundCentrality;
+        }
+
+        public int getPolishMultStartPoints()
+        {
+            return _polishMultStartPoints;
+        }
+
+        public int getMinCalibrationCount()
+        {
+            return _minCalibrationCount;
+        }
+
+        public double getImprovementRatio()
+        {
+            return _improvementRatio;
+        }
+
+        public double getExhaustiveImprovementLimit()
+        {
+            return _exhaustiveImprovementLimit;
+        }
+
+        public double getAicCutoff()
+        {
+            return _aicCutoff;
+        }
+
+        public double getzScoreCutoff()
+        {
+            return _zScoreCutoff;
+        }
+
+        public int getBlockSize()
+        {
+            return _blockSize;
+        }
+
+        public int getThreadBlockSize()
+        {
+            return _threadBlockSize;
+        }
+
+        public boolean isValidate()
+        {
+            return _validate;
+        }
+
+        public ItemSettingsBuilder setRand(Random _rand)
+        {
+            this._rand = _rand;
+            return this;
+        }
+
+        public ItemSettingsBuilder setRandomShuffle(boolean _randomShuffle)
+        {
+            this._randomShuffle = _randomShuffle;
+            return this;
+        }
+
+        public ItemSettingsBuilder setUseThreading(boolean _useThreading)
+        {
+            this._useThreading = _useThreading;
+            return this;
+        }
+
+        public ItemSettingsBuilder setApproximateDerivatives(boolean _approximateDerivatives)
+        {
+            this._approximateDerivatives = _approximateDerivatives;
+            return this;
+        }
+
+        public ItemSettingsBuilder setPolishStartingParams(boolean _polishStartingParams)
+        {
+            this._polishStartingParams = _polishStartingParams;
+            return this;
+        }
+
+        public ItemSettingsBuilder setAllowInteractionCurves(boolean _allowInteractionCurves)
+        {
+            this._allowInteractionCurves = _allowInteractionCurves;
+            return this;
+        }
+
+        public ItemSettingsBuilder setBoundCentrality(boolean _boundCentrality)
+        {
+            this._boundCentrality = _boundCentrality;
+            return this;
+        }
+
+        public ItemSettingsBuilder setPolishMultStartPoints(int _polishMultStartPoints)
+        {
+            this._polishMultStartPoints = _polishMultStartPoints;
+            return this;
+        }
+
+        public ItemSettingsBuilder setMinCalibrationCount(int _minCalibrationCount)
+        {
+            this._minCalibrationCount = _minCalibrationCount;
+            return this;
+        }
+
+        public ItemSettingsBuilder setImprovementRatio(double _improvementRatio)
+        {
+            this._improvementRatio = _improvementRatio;
+            return this;
+        }
+
+        public ItemSettingsBuilder setExhaustiveImprovementLimit(double _exhaustiveImprovementLimit)
+        {
+            this._exhaustiveImprovementLimit = _exhaustiveImprovementLimit;
+            return this;
+        }
+
+        public ItemSettingsBuilder setAicCutoff(double aicCutoff_)
+        {
+            if (aicCutoff_ > 0.0)
+            {
+                throw new IllegalArgumentException("The AIC cutoff must be negative: " + aicCutoff_);
+            }
+
+            this._aicCutoff = aicCutoff_;
+            return this;
+        }
+
+        public ItemSettingsBuilder setzScoreCutoff(double zScoreCutoff_)
+        {
+            this._zScoreCutoff = zScoreCutoff_;
+            return this;
+        }
+
+        public ItemSettingsBuilder setBlockSize(int blockSize_)
+        {
+            this._blockSize = blockSize_;
+            return this;
+        }
+
+        public ItemSettingsBuilder setThreadBlockSize(int threadBlockSize_)
+        {
+            if (threadBlockSize_ < 100)
+            {
+                throw new IllegalArgumentException("Thread block size should not be too small: " + threadBlockSize_);
+            }
+
+            if (this._blockSize < threadBlockSize_)
+            {
+                _blockSize = threadBlockSize_;
+            }
+
+            this._threadBlockSize = threadBlockSize_;
+            return this;
+        }
+
+        public ItemSettingsBuilder setValidate(boolean _validate)
+        {
+            this._validate = _validate;
+            return this;
+        }
+
+    }
+
 }
